1d6560d698ff359e1c1cc4b3bfa3bd15d5745ee6
RuneCodeur <jonathan.larroux@gmail.com>
2024-02-01 17:53:36 +0100

    creation monstre

--- src/functions/classMonster.js
+++ src/functions/classMonster.js
@@ -2,13 +2,35 @@
 
 export default class Monster {
     level = '0';
-    source = '../assets/PNG/Default/';
+    source = './monster/PNG/Default/';
     listLevel = {
         D:'blue',
         E:'yellow',
         S:'green',
-        0:'Black',
-    }
+        0:'dark',
+    };
+    numberEyes= 1;
+    listType = ['A', 'B', 'C', 'D', 'E', 'F'];
+    listEye = [
+        'eye_angry_blue',
+        'eye_angry_green', 
+        'eye_angry_red',
+        'eye_blue',
+        'eye_closed_feminine',
+        'eye_closed_happy',
+        'eye_cute_dark',
+        'eye_cute_light',
+        'eye_dead',
+        'eye_human_blue',
+        'eye_human_green',
+        'eye_human_red',
+        'eye_human',
+        'eye_psycho_dark',
+        'eye_psycho_light',
+        'eye_red',
+        'eye_yellow',
+
+    ];
     name="body_blueD.png";
     x="192";
     y="546";
@@ -17,6 +39,9 @@ export default class Monster {
 
 	constructor() {
         console.log('creation du monstre');
+        
+        this.numberEyes = Math.round(Math.random()*2);
+
 	}
 
     InitLevel(level){
@@ -29,25 +54,29 @@ export default class Monster {
     GETColor(){
         return this.listLevel[this.level];
     }
+
+    GETchest(){
+        let randomChest = Math.round(Math.random()*5);
+        return this.source + 'body_' + this.GETColor() + this.listType[randomChest]+ '.png';
+    }
+
+    GETeye(){
+        let randomEye = Math.round(Math.random()*16);
+        return this.source + this.listEye[randomEye] + '.png';
+    }
     
-    GETName(){
-        return this.name;
+    GETnumberEye(){
+        return this.numberEyes;
     }
 
-    GETX(){
-        return this.x;
+    GETleg(){
+        let randomHead = Math.round(Math.random()*4);
+        return this.source + 'leg_' + this.GETColor() + this.listType[randomHead]+ '.png';
     }
 
-    GETY(){
-        return this.y;
-    }
-
-    GETWidth(){
-        return this.width;
-    }
-
-    GETHeight(){
-        return this.Height;
+    GETarm(){
+        let randomHead = Math.round(Math.random()*4);
+        return this.source + 'arm_' + this.GETColor() + this.listType[randomHead]+ '.png';
     }
     
 }
