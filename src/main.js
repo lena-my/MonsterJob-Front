eb6ff41833ce4f2757a0e6b1accaa487fd18523d
RuneCodeur <jonathan.larroux@gmail.com>
2024-02-01 09:16:03 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import store from './functions/store.js';
+import router from './functions/router.js';
+import { createApp } from 'vue';
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount('#app');
